a7992fb53158ddf29ffb2d1f96f511307a70d5fb
JoacoR7 <joaquin.ruiz2012@gmail.com>
2024-11-13 17:23:30 -0300

    CRUD Inmueble

--- src/main/java/com/uncode/stop/cliente/controller/InmuebleController.java
+++ src/main/java/com/uncode/stop/cliente/controller/InmuebleController.java
@@ -1,10 +1,76 @@
 package com.uncode.stop.cliente.controller;
 
+import com.uncode.stop.cliente.dtos.InmuebleDTO;
+import com.uncode.stop.cliente.dtos.UnidadNegocioDTO;
+import com.uncode.stop.cliente.enums.EstadoInmueble;
+import com.uncode.stop.cliente.services.InmuebleService;
+import com.uncode.stop.cliente.services.UnidadNegocioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/inmueble")
 public class InmuebleController {
+
+    @Autowired
+    private InmuebleService inmuebleService;
+    @Autowired
+    private UnidadNegocioService unidadNegocioService;
+
+    @GetMapping("/listarInmuebles")
+    public String iniciarInmuebles(ModelMap model){
+        List<InmuebleDTO> inmuebles = inmuebleService.listar();
+        model.put("inmuebles", inmuebles);
+        return "/inmueble/inmueble.html";
+    }
+
+    @GetMapping("/editar-inmueble")
+    public String editInmueble(@RequestParam(value = "id", required = false) UUID id,
+                               @RequestParam(value = "accion") String accion,
+                               Model model) {
+
+        InmuebleDTO inmueble = (id == null) ? new InmuebleDTO() : inmuebleService.buscar(id);
+
+        model.addAttribute("inmueble", inmueble);
+        model.addAttribute("accion", accion);
+        model.addAttribute("estadosInmueble", EstadoInmueble.values());
+        model.addAttribute("unidadesDeNegocio", unidadNegocioService.listar());
+
+        return "inmueble/editInmueble";
+    }
+
+    @PostMapping("/actualizar-inmueble")
+    public String actualizarInmueble(@RequestParam(value = "id", required = false) UUID id,
+                                     @RequestParam("numeracion") String numeracion, @RequestParam("piso") String piso,
+                                     @RequestParam("depto") String depto, @RequestParam("estadoInmueble") EstadoInmueble estadoInmueble,
+                                     @RequestParam("unidadDeNegocio") UUID unidadDeNegocioId, @RequestParam("accion") String accion,
+                                     ModelMap model) {
+
+        UnidadNegocioDTO unidadDeNegocio = unidadNegocioService.buscar(unidadDeNegocioId);
+
+        if(id == null) {
+            inmuebleService.crear(numeracion, piso, depto, estadoInmueble, unidadDeNegocio);
+        } else {
+            inmuebleService.modificar(id, numeracion, piso, depto, estadoInmueble, unidadDeNegocio);
+        }
+
+        return "redirect:/inmueble/listarInmuebles";
+
+    }
+
+    @PostMapping("/eliminar-inmueble")
+    public String eliminarInmueble(@RequestParam("id") UUID id) {
+        inmuebleService.eliminar(id);
+        return "redirect:/inmueble/listarInmuebles";
+    }
 
 }
---
+++ src/main/java/com/uncode/stop/cliente/rest/InmuebleDAORest.java
@@ -0,0 +1,60 @@
+package com.uncode.stop.cliente.rest;
+
+import com.uncode.stop.cliente.dtos.InmuebleDTO;
+import com.uncode.stop.cliente.dtos.VisitanteDTO;
+import com.uncode.stop.cliente.entities.ResponseWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class InmuebleDAORest {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String baseUri = "http://localhost:9000/inmuebles";
+
+    public void crear(InmuebleDTO inmueble) {
+        String uri = baseUri;
+        restTemplate.postForEntity(uri, inmueble, InmuebleDTO.class);
+    }
+
+    public void modificar(InmuebleDTO inmueble) {
+        String uri = baseUri + "/" + inmueble.getId();
+        restTemplate.put(uri, inmueble);
+    }
+
+    public void eliminar(UUID id) {
+        String uri = baseUri + "/" + id;
+        restTemplate.delete(uri);
+    }
+
+    public InmuebleDTO buscar(UUID id) {
+        String uri = baseUri + "/" + id;
+        ResponseEntity<InmuebleDTO> response = restTemplate.getForEntity(uri, InmuebleDTO.class);
+        InmuebleDTO inmueble = response.getBody();
+        return inmueble;
+    }
+
+    public List<InmuebleDTO> listar() {
+        String uri = baseUri;
+
+        ResponseEntity<ResponseWrapper<InmuebleDTO>> response = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<ResponseWrapper<InmuebleDTO>>() {}
+        );
+
+        List<InmuebleDTO> listaInmuebles = response.getBody().getContent();
+        return listaInmuebles;
+    }
+
+}
